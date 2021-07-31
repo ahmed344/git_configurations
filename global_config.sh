25d64cdc4f38f967f5392d0d3d3b27988d41c6fc
ahmed344 <ahmedsharaf483@gmail.com>
2021-07-31 04:32:20 +0200

    add global configurations

---
+++ global_config.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+git config --global user.name "ahmed344"
+git config --global user.email "ahmedsharaf483@gmail.com"
+git config --global core.editor "vim"
+git config --global color.ui true
